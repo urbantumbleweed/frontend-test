7256f11d92ba9a023d9e8dd31c762076d2488b9d
Stephen Saunders <saunders.stephen@gmail.com>
2020-01-08 20:59:14 -0800

    Pending Tests for App container

---
+++ src/components/App/App.spec.tsx
@@ -0,0 +1,16 @@
+describe('<App />', () => {
+  describe('initialize', () => {
+    test.todo('the initial window.location is `/restaurants`')
+  })
+  describe('state', () => {
+    test.todo('load `categories` from cache before refetching')
+  })
+  describe('styles', () => {
+    test.todo('`background-color` matches spec')
+    test.todo('`margin` is zero')
+  })
+  describe('render', () => {
+    test.todo('<ListLayout /> when route is `/restaurants`')
+    test.todo('<DetailsLayout /> when route is `/restaurants/:id`')
+  })
+})

77f066073ca6df525ed62ba6c1f95ccc84eaa522
Stephen Saunders <saunders.stephen@gmail.com>
2020-01-08 22:31:50 -0800

    Pending tests for DetailsLayout

---
+++ src/components/DetailsLayout/DetailsLayout.spec.tsx
@@ -0,0 +1,24 @@
+describe('<DetailsLayout />', () => {
+  describe('initialize', () => {
+    test.todo('the initial window.location is `/restaurants/<id>`')
+  })
+  describe('state', () => {
+    test.todo('has a `pageTitle` that is a Restaurant name')
+  })
+  describe('handlers', () => {
+    test.todo('`componentDidMount` calls `fetchReviews`')
+    test.todo('`componentDidMount` calls `fetchBusinessDetails`')
+  })
+  describe('styles', () => {
+    test.todo('`margin-left` matches spec spec value of `64`')
+    test.todo('`margin-top` matches spec spec value of `36`')
+    test.todo('`margin-right` matches spec spec value of `64`')
+  })
+  describe('render', () => {
+    test.todo('<PageHeader />')
+    test.todo('<Rating /> with a prop `variant:large`')
+    test.todo('<LocationView />')
+    test.todo('<Gallery />')
+    test.todo('<ReviewSection />')
+  })
+})
